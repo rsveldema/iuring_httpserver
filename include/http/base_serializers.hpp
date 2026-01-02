6fad79f4daa4a2e3859b7f17315a915b7940348e
Ronald Veldema <rsveldema@gmail.com>
2026-01-02 17:13:10 +0100

    feat: More flexible ser/deser of variants

--- include/http/base_serializers.hpp
+++ include/http/base_serializers.hpp
@@ -18,7 +18,8 @@
     return serialize(t.value);
 }
 
-[[maybe_unused]] static void deserialize(http::hostname_t& t, const json& payload)
+[[maybe_unused]] static void deserialize(
+    http::hostname_t& t, const json& payload)
 {
     std::string s;
     deserialize(s, payload);
@@ -56,31 +57,6 @@
 {
     deserialize(r.value, payload);
 }
-
-#define EMPTY_SER(CODE)                                                        \
-    [[maybe_unused]] static std::string serialize(                             \
-        const http::EmptyObject_##CODE&)                                        \
-    {                                                                          \
-        return "";                                                             \
-    }                                                                          \
-    [[maybe_unused]] static void deserialize(                                  \
-        const http::EmptyObject_##CODE&, const json&)                           \
-    {                                                                          \
-    }
-
-EMPTY_SER(200)
-EMPTY_SER(202)
-EMPTY_SER(204)
-
-EMPTY_SER(307)
-
-EMPTY_SER(403)
-EMPTY_SER(404)
-EMPTY_SER(405)
-EMPTY_SER(409)
-EMPTY_SER(423)
-
-EMPTY_SER(500)
 
 
 [[maybe_unused]] static std::string serialize(const std::string& obj)
@@ -157,6 +133,31 @@ EMPTY_SER(500)
 }
 
 
+#define EMPTY_SER(CODE)                                                        \
+    [[maybe_unused]] static std::string serialize(                             \
+        const http::EmptyObject_##CODE&)                                       \
+    {                                                                          \
+        return "";                                                             \
+    }                                                                          \
+    [[maybe_unused]] static void deserialize(                                  \
+        const http::EmptyObject_##CODE&, const json&)                          \
+    {                                                                          \
+    }
+
+EMPTY_SER(200)
+EMPTY_SER(202)
+EMPTY_SER(204)
+
+EMPTY_SER(307)
+
+EMPTY_SER(403)
+EMPTY_SER(404)
+EMPTY_SER(405)
+EMPTY_SER(409)
+EMPTY_SER(423)
+
+EMPTY_SER(500)
+
 template <typename T>
 [[maybe_unused]] static std::string serialize(const std::optional<T>& obj)
 {
@@ -182,6 +183,42 @@ template <typename T>
     obj = data;
 }
 
+
+template <typename... Types>
+static std::string serialize(const std::variant<Types...>& obj)
+{
+    return std::visit([](const auto& elt) { return serialize(elt); }, obj);
+}
+
+template <std::size_t I, typename... Types>
+static void deserialize_recurs_variant(std::variant<Types...>& obj, const json& payload)
+{
+    using V = std::variant<Types...>;
+    if constexpr (I < std::variant_size_v<V>)
+    {
+        try
+        {
+            using A = std::variant_alternative_t<I, V>;
+
+            A val{};
+            deserialize(val, payload);
+            obj = val;
+            return;
+        }
+        catch (const ParseError& e)
+        {
+            fprintf(stderr, "was not variant alternative %ld\n", I);
+        }
+
+        return deserialize_recurs_variant<I + 1, Types...>(obj, payload);
+    }
+    THROW_ERROR("failed to parse tuple variant");
+}
+template <typename... Types>
+static void deserialize(std::variant<Types...>& obj, const json& payload)
+{
+    deserialize_recurs_variant<0, Types...>(obj, payload);
+};
 
 template <typename T>
 [[maybe_unused]] static std::string serialize(const std::vector<T>& obj)
@@ -252,52 +289,3 @@ template <typename K, typename V>
         obj[jkey] = value;
     }
 }
-
-
-#define TUPLE_SERIALIZERS(A, B)                                                \
-    static std::string serialize(const std::variant<A, B>& obj)                \
-    {                                                                          \
-        switch (obj.index())                                                   \
-        {                                                                      \
-        case 0:                                                                \
-            return serialize(std::get<0>(obj));                                \
-        case 1:                                                                \
-            return serialize(std::get<1>(obj));                                \
-        }                                                                      \
-        INTERNAL_ERROR("internal error");                                      \
-    }                                                                          \
-                                                                               \
-    [[maybe_unused]] static void deserialize(                                  \
-        [[maybe_unused]] std::variant<A, B>& obj,                              \
-        [[maybe_unused]] const json& payload)                                  \
-    {                                                                          \
-        try                                                                    \
-        {                                                                      \
-            A val{};                                                           \
-            deserialize(val, payload);                                         \
-            obj = val;                                                         \
-            return;                                                            \
-        }                                                                      \
-        catch (const ParseError& e)                                            \
-        {                                                                      \
-            fprintf(stderr, "was not alt __field1\n");                         \
-        }                                                                      \
-        try                                                                    \
-        {                                                                      \
-            B val{};                                                           \
-            deserialize(val, payload);                                         \
-            obj = val;                                                         \
-            return;                                                            \
-        }                                                                      \
-        catch (const ParseError& e)                                            \
-        {                                                                      \
-            fprintf(stderr, "was not alt __field0\n");                         \
-        }                                                                      \
-        THROW_ERROR("failed to parse tuple variant");                          \
-    }
-
-
-TUPLE_SERIALIZERS(http::null_t, std::string);
-TUPLE_SERIALIZERS(std::string, http::null_t);
-TUPLE_SERIALIZERS(std::string, bool);
-TUPLE_SERIALIZERS(int64_t, std::string);
